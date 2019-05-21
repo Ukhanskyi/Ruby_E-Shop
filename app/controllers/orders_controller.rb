3923d04aca6e98fd92c692b07f4952a22701a7f9
UKHANSKYI MYKOLA <ukhanskyi@gmail.com>
2019-05-21 18:36:42 +0300

    Update mailer

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 class OrdersController < ApplicationController
-  skip_before_action :authorize, only: [:new, :create]
+  skip_before_action :authorize, only: %i[new create]
 
   include CurrentCart
 
-  before_action :set_cart, only: [:new, :create]
+  before_action :set_cart, only: %i[new create]
   before_action :ensure_cart_isnt_empty, only: :new
-  before_action :set_order, only: [:show, :edit, :update, :destroy]
+  before_action :set_order, only: %i[show edit update destroy]
 
   # GET /orders
   # GET /orders.json
@@ -15,8 +17,7 @@ class OrdersController < ApplicationController
 
   # GET /orders/1
   # GET /orders/1.json
-  def show
-  end
+  def show; end
 
   # GET /orders/new
   def new
@@ -24,8 +25,7 @@ class OrdersController < ApplicationController
   end
 
   # GET /orders/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /orders
   # POST /orders.json
@@ -43,7 +43,10 @@ class OrdersController < ApplicationController
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+        format.json do
+          render json: @order.errors,
+                 status: :unprocessable_entity
+        end
       end
     end
   end
@@ -74,17 +77,19 @@ class OrdersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_order
-      @order = Order.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def order_params
-      params.require(:order).permit(:name, :address, :email, :pay_type)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def order_params
+    params.require(:order).permit(:name, :address, :email, :pay_type)
+  end
 
   private
+
   def ensure_cart_isnt_empty
     if @cart.line_items.empty?
       redirect_to store_index_url, notice: 'Your cart is empty'
--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,11 +1,12 @@
 class OrderMailer < ApplicationMailer
-  default from: 'Sam Ruby <depot@example.com>'
+  default from: 'Mykola Ukhanskyi <dzykan29@gmail.com>'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.order_mailer.received.subject
   #
+
   def received(order)
     @order = order
     mail to: order.email, subject: 'Watches Store Order Confirmation'
@@ -17,9 +18,10 @@ class OrderMailer < ApplicationMailer
   #
   #   en.order_mailer.shipped.subject
   #
+
   def shipped(order)
     @order = order
-    mail to: order.email, subject: 'Watches Store Order Shipped'
+    mail to: order.email, subject: 'Pragmatic Store Order Shipped'
   end
 
 end
